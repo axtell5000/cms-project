ffc299769c4a0994f7db9602cf0c54df18326b16
Stephen Axtell <axtell.stephen@gmail.com>
2019-03-25 11:13:07 +0200

    enabling  the single page post

--- index.php
+++ index.php
@@ -22,6 +22,7 @@
                     
               // Had to put it in the while loop all else everything crashes
               while($row = mysqli_fetch_assoc($select_all_posts_query)) {
+                $post_id = $row['post_id'];
                 $post_title = $row['post_title'];
                 $post_author = $row['post_author'];
                 $post_date = $row['post_date'];
@@ -36,17 +37,19 @@
 
                 <!-- First Blog Post -->
                 <h2>
-                    <a href="#"><?php echo $post_title; ?></a>
+                    <a href="post.php?p_id=<?php echo $post_id; ?>" title="<?php echo $post_title; ?>"><?php echo $post_title; ?></a>
                 </h2>
                 <p class="lead">
                     by <a href="index.php"><?php echo $post_author; ?></a>
                 </p>
                 <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date; ?></p>
                 <hr>
-                <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="<?php echo $post_title; ?>">
+                <a href="post.php?p_id=<?php echo $post_id; ?>" title="<?php echo $post_title; ?>">
+                  <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="<?php echo $post_title; ?>">
+                </a>
                 <hr>
                 <p><?php echo $post_content; ?></p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id; ?>" title="Read more">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
                 <hr>
                 <!-- Post code ends -->
